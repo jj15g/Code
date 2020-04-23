5295a7780ec1f7bb2145623241d40a55e1c59521
John Jacke <jj15g@hpc-login-vm4.rcc.fsu.edu>
2020-04-22 22:59:35 -0400

    Commit

---
+++ Ad-Dif.py
@@ -0,0 +1,36 @@
+import numpy as np
+from numpy import arange
+from matplotlib import pyplot as plt
+#Solving the time-independent advection-diffusion equation
+k = 2.0
+c = 1.0
+
+def AdDif(r):
+	u = r[0]
+	y = r[1]
+	fu = phi
+	fy = -c*fu
+	return np.array([fu,k*k*fy],float)
+phi = 1.0
+a = 0.0
+b = 10.0
+N = 1000
+h = (b-a)/N
+
+tpoints = arange(a,b,h)
+xpoints = []
+ypoints = []
+r = np.array([1.0,1.0],float)
+for t in tpoints:
+	xpoints.append(r[0])
+	ypoints.append(r[1])
+	k1 = h*AdDif(r)
+	k2 = h*AdDif(r+0.5*k1)
+	k3 = h*AdDif(r+0.5*k2)
+	k4 = h*AdDif(r+k3)
+	r += (k1+2*k2+2*k3+k4)/6
+plt.plot(tpoints,xpoints)
+plt.plot(tpoints,ypoints)
+plt.xlabel("t")
+plt.savefig("Ad-Dif.png")
+plt.show()
